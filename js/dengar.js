1cce7406a13569372ca2b91d6f13fc161312b43b
Carlos Eduardo <carled771@gmail.com>
2021-11-24 19:48:33 -0300

    first commit

---
+++ js/dengar.js
@@ -0,0 +1,41 @@
+var escolha
+function escolha(){
+    escolha = prompt('Escolha uma opção: A ou B')
+        if (escolha == 'A') {
+            return location = "./dengar1.html"
+        }
+            else if (escolha == 'B'){
+                return location = "./gameover1.html"
+            }
+                    else {
+                        alert('Digite uma opção válida')
+                    }
+}
+
+var escolha1
+function escolha1(){
+    escolha1 = prompt('Escolha uma opção: A ou B')
+        if (escolha1 == 'A') {
+            return location = "./dengar2.html"
+        }
+            else if (escolha1 == 'B'){
+                return location = "./gameover2.html"
+            }
+                    else {
+                        alert('Digite uma opção válida')
+                    }
+}
+
+var escolha2
+function escolha2(){
+    escolha2 = prompt('Escolha uma opção: A ou B')
+        if (escolha2 == 'A') {
+            return location = "./gameover3.html"
+        }
+            else if (escolha2 == 'B'){
+                return location = "./dengar4.html"
+            }
+                    else {
+                        alert('Digite uma opção válida')
+                    }
+}
---
+++ js/mando.js
@@ -0,0 +1,41 @@
+var escolha
+function escolha(){
+    escolha = prompt('Escolha uma opção: A ou B')
+        if (escolha == 'A') {
+            return location = "./mando1.html"
+        }
+            else if (escolha == 'B'){
+                return location = "./gameover.html"
+            }
+                    else {
+                        alert('Digite uma opção válida')
+                    }
+}
+
+var escolha1
+function escolha1(){
+    escolha1 = prompt('Escolha uma opção: A ou B')
+        if (escolha1 == 'A') {
+            return location = "./gameover1.html"
+        }
+            else if (escolha1 == 'B'){
+                return location = "./mando2.html"
+            }
+                    else {
+                        alert('Digite uma opção válida')
+                    }
+}
+
+var escolha2
+function escolha2(){
+    escolha2 = prompt('Escolha uma opção: A ou B')
+        if (escolha2 == 'A') {
+            return location = "./mando3.html"
+        }
+            else if (escolha2 == 'B'){
+                return location = "./gameover2.html"
+            }
+                    else {
+                        alert('Digite uma opção válida')
+                    }
+}
